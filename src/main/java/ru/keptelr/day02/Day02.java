697d27c9881215f368832d5053dd5d58b989687f
Petr Belyakov <keptelr@ya.ru>
2022-12-07 00:40:02 +0300

    day02 part02 completed

--- src/main/java/ru/keptelr/day02/Day02.java
+++ src/main/java/ru/keptelr/day02/Day02.java
@@ -1,18 +1,27 @@
 package ru.keptelr.day02;
 
 import org.apache.commons.lang3.Range;
+
 import java.util.List;
 
 public class Day02 {
 
-    public static final int[][] intercom = {
-            {1, 2, 3}, //00 01 02
-            {4, 5, 6}, // 10 11 12
-            {7, 8, 9} // 20 21 22
+    private static final int[][] LOCK = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
     };
 
+    private static final String[][] MODERN_LOCK = {
+            {"x", "x", "1", "x", "x"},
+            {"x", "2", "3", "4", "x"},
+            {"5", "6", "7", "8", "9"},
+            {"x", "A", "B", "C", "x"},
+            {"x", "x", "D", "x", "x"},
 
-    public int partOne(List<String> input) {
+    };
+
+    public String partOne(List<String> input) {
         Range<Integer> xRange = Range.between(0, 2);
         Range<Integer> yRange = Range.between(0, 2);
 
@@ -50,14 +59,54 @@ public class Day02 {
                     }
                 }
             }
-            result += String.valueOf(intercom[y][x]);
+            result += String.valueOf(LOCK[y][x]);
         }
 
-        return Integer.parseInt(result);
+        return result;
     }
 
-    public int partTwo(List<String> input) {
-        return 0;
+    public String partTwo(List<String> input) {
+        Range<Integer> xRange = Range.between(0, 4);
+        Range<Integer> yRange = Range.between(0, 4);
+
+        String result = "";
+
+        int x = 0;
+        int y = 2;
+
+        for (String line : input) {
+            for (Character character : line.toCharArray()) {
+                switch (character) {
+                    case 'U': {
+                        if (xRange.contains(y - 1) && !MODERN_LOCK[y - 1][x].equals("x")) {
+                            y--;
+                        }
+                        break;
+                    }
+                    case 'D': {
+                        if (xRange.contains(y + 1) && !MODERN_LOCK[y + 1][x].equals("x")) {
+                            y++;
+                        }
+                        break;
+                    }
+                    case 'L': {
+                        if (yRange.contains(x - 1) && !MODERN_LOCK[y][x - 1].equals("x")) {
+                            x--;
+                        }
+                        break;
+                    }
+                    case 'R': {
+                        if (yRange.contains(x + 1) && !MODERN_LOCK[y][x + 1].equals("x")) {
+                            x++;
+                        }
+                        break;
+                    }
+                }
+            }
+            result += MODERN_LOCK[y][x];
+        }
+
+        return result;
     }
 
 }
--- src/test/java/ru/keptelr/day02/Day02Test.java
+++ src/test/java/ru/keptelr/day02/Day02Test.java
@@ -8,11 +8,11 @@ public class Day02Test extends TestCase {
 
     public void testPartOne() {
         Day02 day02 = new Day02();
-        Assert.assertEquals(1985, day02.partOne(InputParserUtil.readFile("day02")));
+        Assert.assertEquals("1985", day02.partOne(InputParserUtil.readFile("day02")));
     }
 
     public void testPartTwo() {
         Day02 day02 = new Day02();
-        Assert.assertEquals(0, day02.partTwo(InputParserUtil.readFile("day02")));
+        Assert.assertEquals("5DB3", day02.partTwo(InputParserUtil.readFile("day02")));
     }
 }
